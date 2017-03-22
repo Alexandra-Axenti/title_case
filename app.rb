c566f00c8fdb348793d38ca1518742960c92d39e
Alexandra-Axenti <axentialexandra@yahoo.com>
2017-03-22 11:37:32 +0800

    record the first spec test and code

---
+++ app.rb
@@ -0,0 +1,15 @@
+require('sinatra')
+  require('sinatra/reloader')
+  also_reload('lib/**/*.rb')
+  require('./lib/title_case')
+  require('pry')
+  require('launchy')
+
+  get('/') do
+    erb(:form)
+  end
+
+  get('/title') do
+    @title = params.fetch('title').title_case()
+    erb(:title)
+  end
